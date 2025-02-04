579d65608cc85e314697b913a0a2a88013f287e2
klapekm <stanislaw@miszczak.eu>
2025-02-04 16:05:01 +0100

    Made some minor changes in the button class

--- button_template.py
+++ button_template.py
@@ -5,7 +5,7 @@ class Button:
 	def __init__(self, x, y, image, scale):
 		width = image.get_width()
 		height = image.get_height()
-		self.image = pygame.transform.scale(image, (int(width*scale), int(height*scale)))
+		self.image = pygame.transform.scale(image, (int(width * scale), int(height * scale)))
 		self.rect = self.image.get_rect()
 		self.rect.topleft = (x, y)
 		self.clicked = False
@@ -14,10 +14,18 @@ class Button:
 		action = False
 		pos = pygame.mouse.get_pos()
 		if self.rect.collidepoint(pos):
-			if pygame.mouse.get_pressed()[0] == 1 and self.clicked == False:
+			if pygame.mouse.get_pressed()[0] == 1 and self.clicked is False:
 				self.clicked = True
 				action = True
 		if pygame.mouse.get_pressed()[0] == 0:
 			self.clicked = False
 		surface.blit(self.image, self.rect)
 		return action
+
+	def swap_image(self, x, y, image, scale):
+		width = image.get_width()
+		height = image.get_height()
+		self.image = pygame.transform.scale(image, (int(width * scale), int(height * scale)))
+		self.rect = self.image.get_rect()
+		self.rect.topleft = (x, y)
+
